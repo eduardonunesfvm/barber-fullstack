d55b1388ca975481898cc3cc24fe0dcd084ed717
edu <aeonszx2@gmail.com>
2026-04-07 02:26:28 -0300

    fix: fix localhost errors

--- barber/page/settings.py
+++ barber/page/settings.py
@@ -156,4 +156,7 @@ REST_FRAMEWORK = {
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS', 'http://localhost,http://127.0.0.1').split(',')
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500",
+    ]
